534b2ae66ac83bba802d503664b49826f5354a38
Sarvesh-02-coder <spideyninja02@gmail.com>
2026-01-04 23:25:57 +0530

    Update API URL to production backend

--- src/store/useWasteStore.ts
+++ src/store/useWasteStore.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { WasteTicket } from '../types';
 import { useAuthStore } from './useAuthStore';
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://wastetrace-bc.onrender.com";
 
 interface WasteState {
   tickets: WasteTicket[];
